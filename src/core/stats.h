2bc245e5ced3daf9783ddc8ee4ff69573dbbca0e
takashiro <takashiro@qq.com>
2012-05-07 17:30:26 +0800

    Add comments for stats
    Signed-off-by: takashiro <takashiro@qq.com>

--- src/core/stats.h
+++ src/core/stats.h
@@ -12,26 +12,26 @@ class Stats: public QObject{
 	Q_OBJECT
 
 public:
-	Stats();
+	Stats();	//初始化时默认加载今日记录
 	~Stats();
 	static Stats *getInstance();
 
-	void updateTodayLog(WritingHabit &value);
-	void updateLog(QString date, WritingHabit &plus);
-	bool uploadLogs() const;
+	void updateTodayLog(WritingHabit &value);			//更新今日记录，参数为变化量
+	void updateLog(QString date, WritingHabit &plus);	//更新date(ISO标准格式yyyy-mm-dd)日期下的记录，plus为变化量
+	bool uploadLogs() const;							//将记录上传到服务器
 
-	QMap<QString, WritingHabit> getLogs() const;
-	WritingHabit getLog(QString date);
-	WritingHabit getTodayLog() const;
+	QMap<QString, WritingHabit> getLogs() const;		//获得当前加载的全部记录
+	WritingHabit getLog(QString date);					//获得date(ISO标准格式yyyy-mm-dd)日期下的记录
+	WritingHabit getTodayLog() const;					//获得今日记录
 
-	bool loadData(QString date);
+	bool loadData(QString date);						//加载date(ISO标准格式yyyy-mm-dd)日期的记录，加载失败返回false
 
 public slots:
-	void updateTodayLog(HabitType type);
+	void updateTodayLog(HabitType type);				//更新今日记录下的其中一项（该项数量自增1）
 
 private:
-	QMap<QString, WritingHabit> data;
-	WritingHabit *today;
+	QMap<QString, WritingHabit> data;					//存储当前加载到内存的全部数据
+	WritingHabit *today;								//指向当日数据的指针
 };
 
 extern Stats *stats;
